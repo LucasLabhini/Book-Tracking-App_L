110b136d19baa4015f7a373aea320c7740b53a92
daveg <davegalea27@gmail.com>
2025-10-30 09:09:23 +0100

    updated pager.h

--- backend/pager.h
+++ backend/pager.h
@@ -4,6 +4,8 @@
 #include <optional>
 #include "nlohmann/json.hpp"
 
+using json = nlohmann::json;
+
 enum class Status { Reading, Read, Wish };
 
 struct Book {
@@ -13,26 +15,21 @@ struct Book {
     std::string datePublished;
     std::string genre;
     int pageProgress = 0;
-    
     bool inLibrary = true;
     Status section;
 
     static Book fromJSON(const nlohmann::json& j);
-    nlohmann::json toJSON() const;
+    json toJSON() const;
 };
 
 namespace Books {
-
+    Book* findBook(int id);
     bool addBook(const Book& book, Status section);
     bool removeBook(int id);
     bool changeSection(int id, Status newSection);
     bool updateProgress(int id, int page);
-
     std::vector<Book> getBooks(std::optional<Status> section = std::nullopt);
-
     enum class SortBy { Title, Author, Date, Genre };
     std::vector<Book> getBooksSorted(std::optional<Status> section, SortBy sortBy);
-
     std::vector<Book> searchBooks(const std::string& term, std::optional<Status> section = std::nullopt);
-
 }
